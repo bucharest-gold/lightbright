2398d92fe09d7b9962b28e738409d2982e4b1db7
Lance Ball <lball@redhat.com>
2016-11-09 13:35:39 -0500

    examples(stacktrace): Illustrate various stacks based on different usage.

--- example/stacktrace/index.js
+++ example/stacktrace/index.js
@@ -1,8 +1,6 @@
-require('clarify');
 
 const LightBright = require('../../');
 LightBright.addFilter(LightBright.builtins.stacktrace);
-LightBright.enable();
 
 function somethingAsync () {
   setTimeout(
@@ -13,12 +11,36 @@ function somethingAsync () {
 
 function callsSomethingAsync () {
   somethingAsync();
+  setTimeout(() => {
+    console.log(`Now, we enable LightBright.builtins.stacktrace and
+see what the exception looks like. Notice we now have call sites from our own source.`);
+    LightBright.enable();
+    alsoCallsSomethingAsync();
+  }, 20);
 }
 
+function alsoCallsSomethingAsync () {
+  somethingAsync();
+  setTimeout(() => {
+    console.log(`Finally, add the 'clarify' module to eliminate all
+node core stack lines that we typically don't care about. Just right!`);
+    require('clarify');
+    stillCallsSomethingAsync();
+  }, 30);
+}
+
+function stillCallsSomethingAsync () {
+  somethingAsync();
+}
+
+console.log(`Before enabling LightBright.builtins.stacktrace,
+have a look at what an async exception stack looks like`);
 callsSomethingAsync();
 
+
 process.on('uncaughtException', (e) => {
-  process._rawDebug('UncaughtException');
+  process._rawDebug('\n------------------------');
   process._rawDebug(e);
+  process._rawDebug('------------------------\n');
 });
 
